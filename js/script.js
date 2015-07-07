0fa0e49e96f7e6bb2efce6a653f0d38c7ffdf14f
Max Tsoy <miniraptorkimep@gmail.com>
2015-07-07 15:10:38 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MiniRaptor on 28.06.2015.
+ */
+$(function(){
+    $('#wrapper').noisy({
+        intensity: 0.9, //интенсивность шума
+        size: 200, // размер
+        opacity: 0.08, // прозрачность
+        monochrome: false // одноцветный шум
+    });
+});
+
